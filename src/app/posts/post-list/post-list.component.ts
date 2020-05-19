8cbe9b63feba190ef96a9784eed269c18b198c28
sserges <agounkeserge@gmail.com>
2020-05-19 14:33:44 +0000

    Diving Into Structural Directives

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css'],
+})
+export class PostListComponent implements OnInit {
+  // posts = [
+  //   { title: 'First Post', content: 'This is the first post\'s content' },
+  //   { title: 'Second Post', content: 'This is the first post\'s content' },
+  //   { title: 'Third Post', content: 'This is the first post\'s content' }
+  // ];
+
+  posts = [];
+
+  constructor() { }
+
+  ngOnInit(): void { }
+}
