0b100e50313ae1da7ee3b1ca49d8d86fb9aeabf6
sserges <agounkeserge@gmail.com>
2020-05-20 11:34:04 +0000

    Creating Posts with Property & Event Binding

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,20 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css'],
 })
-export class PostListComponent implements OnInit {
-  // posts = [
-  //   { title: 'First Post', content: 'This is the first post\'s content' },
-  //   { title: 'Second Post', content: 'This is the first post\'s content' },
-  //   { title: 'Third Post', content: 'This is the first post\'s content' }
-  // ];
+export class PostListComponent {
 
-  posts = [];
+  @Input() posts = [];
 
   constructor() { }
-
-  ngOnInit(): void { }
 }
