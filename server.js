ef61a47e705c76ec396f422b642332b11b1ef5ec
sserges <agounkeserge@gmail.com>
2020-06-03 18:43:40 +0000

    Adding the express framework

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const http = require('http');
 
-const server = http.createServer((req, res) => {
-  res.end('This is my first response');
-});
+const app = require('./backend/app');
 
-server.listen(process.env.PORT || 3000);
+const port = process.env.PORT || 3000;
+
+const server = http.createServer(app);
+
+server.listen(port);
