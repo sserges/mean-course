0d02d808e0548905fb260aa530606a9e58ac7a18
sserges <agounkeserge@gmail.com>
2020-06-19 17:52:34 +0000

    Connecting our Node Express App to MongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const Post = require('./models/post');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://max:93gVM2uO9q86MkTN@cluster0-p34fz.mongodb.net/mean-course?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('Connected to database');
+}).catch(() => {
+  console.log('Connection failed');
+});
 
 app.use(bodyParser.json());
 
