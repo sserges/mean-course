3f85a09e40b6ef73b57e75b143381cb52a24487b
sserges <agounkeserge@gmail.com>
2020-08-12 18:24:43 +0000

    Updating Posts on the Server

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -38,7 +38,7 @@ export class PostsService {
   }
 
   getPost(id: string) {
-    return { ...this.posts.find(p => p.id === id) };
+    return this.httpClient.get<{ _id: string, title: string, content: string }>(`http://localhost:3000/api/posts/${id}`);
   }
 
   addPost(title: string, content: string) {
@@ -56,7 +56,13 @@ export class PostsService {
   updatePost(id: string, title: string, content: string) {
     const post: Post = { id, title, content };
     this.httpClient.put(`http://localhost:3000/api/posts/${id}`, post).subscribe(
-      response => console.log(response)
+      response => {
+        const updatedPosts = [...this.posts];
+        const oldPostIndex = updatedPosts.findIndex(p => p.id === post.id);
+        updatedPosts[oldPostIndex] = post;
+        this.posts = updatedPosts;
+        this.postsUpdated.next([...this.posts]);
+      }
     );
   }
 
