08d31c3c5087dc4cd0e561bdcf19fa7ced219bd7
sserges <agounkeserge@gmail.com>
2020-08-24 17:26:53 +0000

    Adding Middleware to Protect Routes

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -2,6 +2,7 @@ const express = require('express');
 const multer = require('multer');
 
 const Post = require('../models/post');
+const checkAuth = require('../middlewares/check-auth');
 
 const router = express.Router();
 
@@ -29,7 +30,7 @@ const storage = multer.diskStorage({
   }
 });
 
-router.post('/', multer({ storage }).single("image"), (req, res, next) => {
+router.post('/', checkAuth, multer({ storage }).single("image"), (req, res, next) => {
   const url = req.protocol + '://' + req.get('host');
   const post = new Post({
     title: req.body.title,
@@ -52,7 +53,7 @@ router.post('/', multer({ storage }).single("image"), (req, res, next) => {
 
 });
 
-router.put('/:id', multer({ storage }).single("image"), (req, res, next) => {
+router.put('/:id', checkAuth, multer({ storage }).single("image"), (req, res, next) => {
   let imagePath = req.body.imagePath;
   if (req.file) {
     const url = req.protocol + '://' + req.get('host');
@@ -104,7 +105,7 @@ router.get('/:id', (req, res, next) => {
   });
 });
 
-router.delete('/:id', (req, res, next) => {
+router.delete('/:id', checkAuth, (req, res, next) => {
   Post.deleteOne({ _id: req.params.id }).then(
     result => {
       res.status(200).json({ message: 'Post deleted!' });
